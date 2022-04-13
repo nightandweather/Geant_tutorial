689a82d679f450dc8ccfc0df97bcdb8d33de290b
Lee Kang Houn <48376964+nightandweather@users.noreply.github.com>
2022-04-13 18:22:37 +0900

    geant4 tutorial following

---
+++ tutorial_4/build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/Geant4/share/Geant4-10.7.2/geant4make/sim/tutorial_4/construction.cc" "/opt/Geant4/share/Geant4-10.7.2/geant4make/sim/tutorial_4/build/CMakeFiles/sim.dir/construction.cc.o"
+  "/opt/Geant4/share/Geant4-10.7.2/geant4make/sim/tutorial_4/physics.cc" "/opt/Geant4/share/Geant4-10.7.2/geant4make/sim/tutorial_4/build/CMakeFiles/sim.dir/physics.cc.o"
+  "/opt/Geant4/share/Geant4-10.7.2/geant4make/sim/tutorial_4/sim.cc" "/opt/Geant4/share/Geant4-10.7.2/geant4make/sim/tutorial_4/build/CMakeFiles/sim.dir/sim.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/CLHEP/lib/CLHEP-2.4.4.2/../../include"
+  "/opt/Geant4/include/Geant4"
+  "/opt/CLHEP/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
